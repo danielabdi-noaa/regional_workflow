653b39ef31caad480d3482cf64f47b00ebd23993
Daniel Abdi <daniel.abdi@noaa.gov>
2022-08-23 16:08:15 +0000

    Add post_step.

--- scripts/exregional_run_fcst.sh
+++ scripts/exregional_run_fcst.sh
@@ -505,6 +505,7 @@ PREP_STEP
 eval ${RUN_CMD_FCST} ${FV3_EXEC_FP} ${REDIRECT_OUT_ERR} || print_err_msg_exit "\
 Call to executable to run FV3-LAM forecast returned with nonzero exit
 code."
+POST_STEP
 #
 #-----------------------------------------------------------------------
 #
